f65c123a20123b0883a69842eceef92451d6e3db
JunDev1 <86519269+JunDev1@users.noreply.github.com>
2023-02-21 00:46:04 +0300

    Initial commit

---
+++ app/src/main/java/com/example/service/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.example.service
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import com.example.service.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        binding.simpleService.setOnClickListener {
+            startService(MyService.newIntent(this, 25))
+            stopService(MyForegroundService.newIntent(this))
+        }
+        binding.foregroundService.setOnClickListener {
+            ContextCompat.startForegroundService(this, MyForegroundService.newIntent(this))
+        }
+    }
+}
